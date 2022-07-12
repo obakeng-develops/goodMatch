2a5b03ef5fffcacedbbf7f7467d192b1de6f9032
Obakeng <mosadiobakeng7@gmail.com>
2022-07-12 17:14:26 +0200

    add find percentage variable

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ function matchParticipants(participant1, participant2) {
 
     let fullText = participant1 + " matches " + participant2;
     let charMapping = createCharMapping(fullText);
-
+    let frequencyResult = createFrequencyResult(charMapping);
+    let percentage = findPercentage(0, frequencyResult.length - 1, frequencyResult);
 }
