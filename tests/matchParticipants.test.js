d63775c33acc7cf1bf0a81f10732f5cd9f827023
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:49:23 +0200

    test: added first test case

--- tests/matchParticipants.test.js
+++ tests/matchParticipants.test.js
@@ -2,4 +2,9 @@ import { matchParticipants } from "../main";
 
 test('match the participants', () => {
 
+    let firstParticipant = "Jack";
+    let secondParticipant = "Jill";
+
+    expect(matchParticipants("Jack", "Jill")).toEqual("Jack matches Jill 60%");
+
 });
