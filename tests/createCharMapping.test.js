89de46b13cb38732e54868b8e1418fce4671fa21
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:01:14 +0200

    task: add expected value

--- tests/createCharMapping.test.js
+++ tests/createCharMapping.test.js
@@ -11,7 +11,18 @@ test('creates a character mapping from string', () => {
     let fullText = "magic matches johnson";
 
     expect(createCharMapping(fullText)).toEqual({
-        
+        m: 2,
+        a: 2,
+        g: 1,
+        i: 1,
+        c: 2,
+        t: 1,
+        h: 2,
+        e: 1,
+        s: 2,
+        j: 1,
+        o: 2,
+        n: 2
     });
 
 });
