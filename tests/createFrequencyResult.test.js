1e69235b9c699441f921d566fedd61d34dfa5307
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:09:40 +0200

    task: add test for creating fied first test case

--- tests/createFrequencyResult.test.js
+++ tests/createFrequencyResult.test.js
@@ -2,4 +2,21 @@ import { createFrequencyResult } from "../main";
 
 test('creates initial number from character mapping', () => {
 
+    let firstCharMap = {
+        m: 2,
+        a: 2,
+        g: 1,
+        i: 1,
+        c: 2,
+        t: 1,
+        h: 2,
+        e: 1,
+        s: 2,
+        j: 1,
+        o: 2,
+        n: 2
+    }
+
+    expect(createFrequencyResult(firstCharMap)).toBe('221121212122');
+
 });
