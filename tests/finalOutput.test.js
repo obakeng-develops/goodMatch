93373c05112302ac8933be8dea4c975a0711e146
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:34:05 +0200

    task: add first test case

--- tests/finalOutput.test.js
+++ tests/finalOutput.test.js
@@ -2,4 +2,10 @@ import { finalOutput } from "../main";
 
 test('output final text using participant names and percentage', () => {
 
+    let firstParticipant = "Jack";
+    let secondParticipant = "Jill";
+    let firstOutput = finalOutput(firstParticipant, secondParticipant, '60');
+
+    expect(firstOutput).toBe('Jack matches Jill 60%');
+
 });
