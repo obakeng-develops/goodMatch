22adc075ea3967799bbcab33ca2fc906f6631c5f
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:24:54 +0200

    tasl: third test case

--- tests/findPercentage.test.js
+++ tests/findPercentage.test.js
@@ -10,4 +10,8 @@ test('find percentage of initial number', () => {
 
     expect(findPercentage(0, secondStringResult.length - 1, secondStringResult)).toBe('82');
 
+    let thirdStringResult = '3412111221';
+
+    expect(findPercentage(0, thirdStringResult.length - 1, thirdStringResult)).not.toBe('21')
+
 });
