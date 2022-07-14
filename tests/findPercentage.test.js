883ddb7d7313ca574e85a55ef00f15354abb35b7
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:18:10 +0200

    task: create first test case

--- tests/findPercentage.test.js
+++ tests/findPercentage.test.js
@@ -2,4 +2,8 @@ import { findPercentage } from '../main';
 
 test('find percentage of initial number', () => {
 
+    let firstStringResult = '22211111112';
+
+    expect(findPercentage(0, firstStringResult.length - 1, firstStringResult)).toBe('60');
+
 });
