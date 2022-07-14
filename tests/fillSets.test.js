444321850ca0c4d45eb16fd034cbfc288829ea8d
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 12:09:49 +0200

    task: added first test case

--- tests/fillSets.test.js
+++ tests/fillSets.test.js
@@ -2,4 +2,21 @@ import { fillSets } from "../readFile";
 
 test('fill set groups', () => {
 
+    let firstSet = new Set();
+    let secondSet = new Set();
+    let firstArray = [
+        ['Kimberley, f'],
+        ['Jason, m'],
+        ['Billy, m'],
+        ['Trini, f'],
+        ['Tommy, m'],
+        ['Zack, m'],
+        ['Billy, f'],
+        ['Jason, m']
+    ]
+
+    fillSets(firstSet, secondSet, firstArray);
+
+    expect(firstSet).toEqual(new Set('Kimberley', 'Trini', 'Billy'));
+
 });
