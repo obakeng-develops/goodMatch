9351073840d3bc9e42ead360b15a249ff3346999
Obakeng <mosadiobakeng7@gmail.com>
2022-07-14 11:11:49 +0200

    task: add second test case

--- tests/createFrequencyResult.test.js
+++ tests/createFrequencyResult.test.js
@@ -19,4 +19,23 @@ test('creates initial number from character mapping', () => {
 
     expect(createFrequencyResult(firstCharMap)).toBe('221121212122');
 
+    let secondCharMap = {
+        n: 2,
+        o: 2,
+        v: 3,
+        a: 3,
+        c: 3,
+        m: 2,
+        t: 2,
+        h: 2,
+        e: 2,
+        s: 2,
+        d: 2,
+        j: 2,
+        k: 2,
+        i: 2,
+    }
+
+    expect(createFrequencyResult(secondCharMap)).toBe('22333222222222');
+
 });
